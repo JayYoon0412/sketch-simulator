7d19c906ebcdb4cc43301109d02c8fd120d7df29
Jay Yoon <jiwon.yoon@duke.edu>
2021-10-11 22:13:29 -0400

    need to add remove and redraw

--- myscript.js
+++ myscript.js
@@ -1,11 +1,32 @@
 const container = document.querySelector(".container");
-for (let k=0; k<16; k++) {
-    let columnBox = document.createElement("div");
-    columnBox.classList.add("columnBox");
-    container.appendChild(columnBox);
-    for (let i=0; i<16; i++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        columnBox.appendChild(box);
+const reset = document.querySelector(".reset");
+let num = 16;
+draw(num);
+
+function draw(a) {
+    for (let k=0; k<a; k++) {
+        let columnBox = document.createElement("div");
+        columnBox.classList.add("columnBox");
+        container.appendChild(columnBox);
+        columnBox.setAttribute("style", `height: ${500/num}px`);
+        for (let i=0; i<a; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            columnBox.appendChild(box);
+            box.setAttribute("style", `width: ${500/num}px; height: ${500/num}px`);
+        }
     }
 }
+
+function pixify(e) {
+    e.target.classList.add("colored");
+}
+
+function gridSet() {
+    let num2 = parseInt(window.prompt("Enter grid dimension: ", ""));
+    draw(num2);
+}
+
+reset.addEventListener("click", gridSet);
+const boxes = Array.from(document.querySelectorAll(".box"));
+boxes.forEach (box => box.addEventListener("mouseover", pixify));
